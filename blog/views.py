28fe13121ae4889ced250c4a02ff651691e7cb5a
John Doe <johndoe@example.com>
2025-08-11 08:45:06 +0530

    Second day of learning Django, how to work with Databse using ORM

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'title': 'My First Blog 1',
-        'author': 'Wasim Akram',
-        'content': 'This is my first blog',
-        'date_posted': '10th August 2025'
-    },
-    {
-        'title': 'My First Blog 2',
-        'author': 'Wasim Akram',
-        'content': 'This is my Second blog',
-        'date_posted': '11th August 2025'
-    }    
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
